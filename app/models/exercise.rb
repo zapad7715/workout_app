e575b5c92b836a4409056a0182bcc16eb3453869
zapad7715 <zapad7715@ya.ru>
2016-02-26 14:21:25 +0300

    fix some problem

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -7,5 +7,5 @@ class Exercise < ActiveRecord::Base
   validates :workout_date, presence: true
   validates :user_id, presence: true
 
-  default_scope { where('workout_date > ?', 7.days.ago).order(workout_date: :desc) }
+  default_scope { where('workout_date > ?', 30.days.ago).order(workout_date: :desc) }
 end
